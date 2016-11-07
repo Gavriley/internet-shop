247c51f59064863d8e6e1505816a73d43fda58bf
darkness <gavrileypetro@gmail.com>
2016-11-07 16:57:19 +0200

    Start Pay

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,6 +4,19 @@ class ProductsController < ApplicationController
 	def index
 		@title = "Головна"
 		@products = Product.latest
+
+		# require 'liqpay'
+		# liqpay = Liqpay.new
+		# res = liqpay.api 'invoice/send', { email: 'gavrileypetro@gmail.com', amount: 100, currency: 'UAH',
+		#   order_id: 'qwerty_12', 
+		#   sandbox: 1,
+		#   goods: [{
+		#               amount: 100,
+		#               count: 6,
+		#               unit: 'шт.',
+		#               name: 'Lorem' }]}
+
+		# File.open('/home/darkness/insilico/log.txt', 'w') { |f| f << res }              
 	end
 	
 	def show
---
+++ config/initializers/liqpay.rb
@@ -0,0 +1,4 @@
+::Liqpay.configure do |config|
+  config.public_key = 'i60051559237'
+  config.private_key = 'tPFRQ7x2v9ExzQ8KA4f8TYumv9CoO4eWQ0BY5r9O'
+end
